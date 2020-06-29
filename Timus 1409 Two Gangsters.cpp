2a98f202c361d4fe5daae8bfc9c4d30515040b66
DevJewel <58494211+DevJewel143@users.noreply.github.com>
2020-06-30 05:28:02 +0600

    Add files via upload
    1409. Two Gangsters
    Problem Link : https://acm.timus.ru/problem.aspx?space=1&num=1409

---
+++ Timus 1409 Two Gangsters.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define Om ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+    Om;
+    int a, b;
+    cin>>a>>b;
+    cout<<--b<<" "<<--a<<endl;
+    return 0;
+}
+
+
