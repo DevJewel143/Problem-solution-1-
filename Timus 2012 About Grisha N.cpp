0152df9adf6cf733cfa7325f7d54541cb50d4bbc
DevJewel <58494211+DevJewel143@users.noreply.github.com>
2020-06-30 05:02:40 +0600

    Add files via upload
    Timus 2012 About Grisha N
    problem link : https://acm.timus.ru/problem.aspx?space=1&num=2012

---
+++ Timus 2012 About Grisha N.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define Om ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+    Om;
+    int f;
+    cin>>f;
+    if( (12-f)*45 <= 4*60 )
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+    return 0;
+}
+
+
