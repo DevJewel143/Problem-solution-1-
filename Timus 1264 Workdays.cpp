8ab60753980f21c83d8343f53a7f6d26f5258a35
DevJewel <58494211+DevJewel143@users.noreply.github.com>
2020-06-29 20:50:02 +0600

    Add files via upload
    Timus 1264 Workdays
    Problem link : https://acm.timus.ru/problem.aspx?space=1&num=1264

---
+++ Timus 1264 Workdays.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define Om ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+    Om;
+    int n, m;
+    cin>>n>>m;
+    cout<<n*(m+1)<<endl;
+    return 0;
+}
+
+
