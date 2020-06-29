ea41b4c91baa5e3599cc7934cac16cae47c1a97f
DevJewel <58494211+DevJewel143@users.noreply.github.com>
2020-06-30 05:16:26 +0600

    Add files via upload
    Timus 1293 Eniya
    Problem link : https://acm.timus.ru/problem.aspx?space=1&num=1293

---
+++ Timus 1293 Eniya.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define Om ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+    Om;
+    cout<<"Hello World"<<endl;
+
+    return 0;
+}
+
