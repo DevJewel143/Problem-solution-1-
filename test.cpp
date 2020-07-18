e59e7d1aa5dd7344cf86da26d70c8221f6873147
DevJewel <58494211+DevJewel143@users.noreply.github.com>
2020-07-18 13:00:54 +0600

    Add files via upload
    12412 - A Typical Homework (a.k.a Shi Xiong Bang Bang Mang)
    Problem link : https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3843
    Copy Code

--- test.cpp
+++ test.cpp
@@ -1,53 +1,257 @@
-#include <stdio.h>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
 
-int main()
-{
-    int i, n, k, m;
-    int next[30], pre[30];
-    while(scanf("%d %d %d", &n, &k, &m) == 3)
-    {
-        if(n == 0 && k == 0 && m == 0)
-            break;
-        for(i = 1; i <= n; i++)
-            next[i] = i+1, pre[i] = i-1;
-        next[n] = 1, pre[1] = n;
-        int idx1 = 1, idx2 = n, used[30] = {};
-        int flag = 0;
-        while(n)
-        {
-            for(i = 1; i < k; i++)
-                idx1 = next[idx1];
-            for(i = 1; i < m; i++)
-                idx2 = pre[idx2];
-            if(flag)
-                putchar(',');
-            flag = 1;
-            if(idx1 != idx2)
-            {
-                printf("%3d%3d", idx1, idx2);
-                used[idx1] = used[idx2] = 1;
-                n -= 2;
-                next[pre[idx1]] = next[idx1];
-                pre[next[idx1]] = pre[idx1];
-                next[pre[idx2]] = next[idx2];
-                pre[next[idx2]] = pre[idx2];
-            }
-            else
-            {
-                used[idx1] = 1;
-                printf("%3d", idx1);
-                next[pre[idx1]] = next[idx1];
-                pre[next[idx1]] = pre[idx1];
-                n--;
-            }
-            if(n == 0)
-                break;
-            while(used[idx1])
-                idx1 = next[idx1];
-            while(used[idx2])
-                idx2 = pre[idx2];
-        }
-        puts("");
-    }
-    return 0;
+using std::string;
+
+struct Student {
+	string SID, CID;
+	string name;
+
+	int rank, position, numberOfPassed;
+	double sum, average;
+
+	int score[4]; // 0->Chinese, 1->Math, 2->English, 3->Program
+
+	Student(string SID, string CID, string name, int *score) : SID(SID), CID(CID), name(name), rank(0), sum(0), average(0) {
+		for (int i = 0; i < 4; i++) this->score[i] = score[i];
+		for (int i = 0; i < 4; i++) this->sum += this->score[i];
+		this->average = this->sum / 4.0;
+		this->position = 0;
+		numberOfPassed = 0;
+	}
+
+	bool operator<(const Student &a) const {
+		return sum > a.sum;
+	}
+
+	void calc() {
+		for (int i = 0; i < 4; i++) if (score[i] >= 60) numberOfPassed++;
+	}
+};
+
+std::vector<Student> v;
+std::set<string> s;
+
+void printMenu() {
+	using namespace std;
+
+	cout << "Welcome to Student Performance Management System (SPMS)." << endl << endl;
+	cout << "1 - Add" << endl;
+	cout << "2 - Remove" << endl;
+	cout << "3 - Query" << endl;
+	cout << "4 - Show ranking" << endl;
+	cout << "5 - Show Statistics" << endl;
+	cout << "0 - Exit" << endl << endl;
+}
+
+void add() {
+	using namespace std;
+
+	while (true) {
+		cout << "Please enter the SID, CID, name and four scores. Enter 0 to finish." << endl;
+		string SID, CID;
+		string name;
+		int score[4];
+
+		cin >> SID;
+		if (SID == "0") break;
+
+		cin >> CID >> name;
+		for (int i = 0; i < 4; i++) {
+			cin >> score[i];
+		}
+
+		if (s.count(SID)) cout << "Duplicated SID." << endl;
+		else  {
+			Student x(SID, CID, name, score);
+			x.calc();
+			v.push_back(x);
+			s.insert(SID);
+		}
+	}
+}
+
+void remove() {
+	using namespace std;
+	while (true) {
+		cout << "Please enter SID or name. Enter 0 to finish." << endl;
+
+		string tmp;
+		cin >> tmp;
+
+		if (tmp == "0") break;
+
+		int sum = 0;
+
+		vector<int> deleted;
+		for (vector<Student>::iterator it = v.begin(); it != v.end(); it++) {
+			if (it->SID == tmp || it->name == tmp) {
+				deleted.push_back(it - v.begin());
+				sum++;
+			}
+		}
+
+		for (vector<int>::iterator it = deleted.begin(); it != deleted.end(); it++) {
+			v.erase(v.begin() + *it);
+			s.erase((v.begin() + *it)->SID);
+		}
+		cout << sum << " student(s) removed." << endl;
+	}
+}
+
+void query() {
+	using namespace std;
+	while (true) {
+		cout << "Please enter SID or name. Enter 0 to finish." << endl;
+
+		string tmp;
+		cin >> tmp;
+
+		if (tmp == "0") break;
+
+		vector<Student> rank;
+		for (vector<Student>::iterator it = v.begin(); it != v.end(); it++) {
+			if (it->SID == tmp || it->name == tmp) {
+				it->position = it - v.begin();
+				// rank.push_back(*it);
+			}
+			rank.push_back(*it);
+		}
+
+		stable_sort(rank.begin(), rank.end());
+		for (vector<Student>::iterator it = rank.begin(); it != rank.end(); it++) {
+			if (it == rank.begin()) v[it->position].rank = 1;
+			else if (it->sum == (it - 1)->sum) v[it->position].rank = v[(it - 1)->position].rank;
+			else v[it->position].rank = it - rank.begin() + 1;
+		}
+
+		for (vector<Student>::iterator it = v.begin(); it != v.end(); it++) {
+			if (it->SID == tmp || it->name == tmp) {
+				cout << it->rank << " ";
+				cout << it->SID << " " << it->CID << " " << it->name << " ";
+				for (int i = 0; i < 4; i++) cout << it->score[i] << " ";
+				cout << it->sum << " " ;
+				printf("%.2lf\n", it->average);
+			}
+		}
+	}
+}
+
+void calcWhole(int lesson, double &average, int &passed, int &notPassed) {
+	using namespace std;
+	int sum = 0;
+	for (vector<Student>::iterator it = v.begin(); it != v.end(); it++) {
+		sum += it->score[lesson];
+		if (it->score[lesson] >= 60) passed++;
+		else notPassed++;
+	}
+	average = (double)(sum) / v.size();
+}
+
+void printLesson(int i) {
+	using namespace std;
+	if (i == 0) cout << "Chinese" << endl;
+	if (i == 1) cout << "Mathematics" << endl;
+	if (i == 2) cout << "English" << endl;
+	if (i == 3) cout << "Programming" << endl;
+}
+
+void statistics() {
+	using namespace std;
+	cout << "Please enter class ID, 0 for the whole statistics." << endl;
+	string tmp;
+	cin >> tmp;
+
+	if (tmp == "0") {
+		for (int i = 0; i < 4; i++) {
+			int passed = 0, notPassed = 0;
+			double average = 0;
+
+			calcWhole(i, average, passed, notPassed);
+
+			printLesson(i);
+
+			printf("Average Score: %.2lf\n", average);
+			cout << "Number of passed students: " << passed << endl;
+			cout << "Number of failed students: " << notPassed << endl << endl;
+		}
+
+		int passedThree = 0, passedTwo = 0, passedOne = 0, failedALL = 0, passedAll = 0;
+		for (vector<Student>::iterator it = v.begin(); it != v.end(); it++) {
+			if (it->numberOfPassed >= 1) passedOne++;
+			if (it->numberOfPassed >= 2) passedTwo++;
+			if (it->numberOfPassed >= 3) passedThree++;
+			if (it->numberOfPassed == 4) passedAll++;
+			if (it->numberOfPassed == 0) failedALL++;
+		}
+
+		cout << "Overall:" << endl;
+		cout << "Number of students who passed all subjects: " << passedAll << endl;
+		cout << "Number of students who passed 3 or more subjects: " << passedOne << endl;
+		cout << "Number of students who passed 2 or more subjects: " << passedTwo << endl;
+		cout << "Number of students who passed 1 or more subjects: " << passedThree << endl;
+		cout << "Number of students who failed all subjects: " << failedALL << endl << endl;
+	} else {
+		for (int i = 0; i < 4; i++) {
+			int sum = 0, passed = 0, notPassed = 0;
+			double average = 0;
+
+			int numberOfStudents = 0;
+			for (vector<Student>::iterator it = v.begin(); it != v.end(); it++) {
+				if (it->CID == tmp || it->SID == tmp) {
+					numberOfStudents++;
+					sum += it->score[i];
+					if (it->score[i] >= 60) passed++;
+					else notPassed++;
+				}
+			}
+
+			average = sum / (double)(numberOfStudents);
+
+			printLesson(i);
+
+			printf("Average Score: %.2lf\n", average);
+			cout << "Number of passed students: " << passed << endl;
+			cout << "Number of failed students: " << notPassed << endl << endl;
+		}
+
+		int passedThree = 0, passedTwo = 0, passedOne = 0, failedALL = 0, passedAll = 0;
+		for (vector<Student>::iterator it = v.begin(); it != v.end(); it++) {
+			if (it->CID == tmp || it->SID == tmp) {
+				if (it->numberOfPassed >= 1) passedOne++;
+				if (it->numberOfPassed >= 2) passedTwo++;
+				if (it->numberOfPassed >= 3) passedThree++;
+				if (it->numberOfPassed == 4) passedAll++;
+				if (it->numberOfPassed == 0) failedALL++;
+			}
+		}
+
+		cout << "Overall:" << endl;
+		cout << "Number of students who passed all subjects: " << passedAll << endl;
+		cout << "Number of students who passed 3 or more subjects: " << passedOne << endl;
+		cout << "Number of students who passed 2 or more subjects: " << passedTwo << endl;
+		cout << "Number of students who passed 1 or more subjects: " << passedThree << endl;
+		cout << "Number of students who failed all subjects: " << failedALL << endl << endl;
+	}
+}
+
+int main() {
+	while (true) {
+		printMenu();
+		int x;
+		std::cin >> x;
+		if (x == 1) add();
+		if (x == 2) remove();
+		if (x == 3) query();
+		if (x == 4) std::cout << "Showing the ranklist hurts students' self-esteem. Don't do that." << std::endl;
+		if (x == 5) statistics();
+		if (x == 0) break;
+	}
+
+	return 0;
 }
